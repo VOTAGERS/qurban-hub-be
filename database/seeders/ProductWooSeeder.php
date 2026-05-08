a3868b95d77a3b5ce71afd4a4ee833b803ae1ff3
Ganes <ganesyudhakusuma@gmail.com>
2026-05-08 14:07:20 +0700

    update seeds data testing like

--- database/seeders/ProductWooSeeder.php
+++ database/seeders/ProductWooSeeder.php
@@ -16,19 +16,19 @@ class ProductWooSeeder extends Seeder
             [
                 'woo_id' => 101,
                 'name' => 'Kambing Qurban Tipe A',
-                'price' => 2500000.00,
+                'price' => 350.00,
                 'status' => 'publish',
             ],
             [
                 'woo_id' => 102,
                 'name' => 'Sapi Limousin Premium',
-                'price' => 25000000.00,
+                'price' => 2800.00,
                 'status' => 'publish',
             ],
             [
                 'woo_id' => 103,
                 'name' => 'Domba Garut Super',
-                'price' => 4000000.00,
+                'price' => 450.00,
                 'status' => 'publish',
             ],
         ];
